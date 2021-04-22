8988e54b69d91f0073ec3fb2b2c3c306b6ac86d8
Kyle <knbedgm@users.noreply.github.com>
2021-04-22 00:47:32 -0400

    Basic Fiddiling done

--- BUFHelper/UserFolders.cs
+++ BUFHelper/UserFolders.cs
@@ -1,9 +1,62 @@
 ﻿using System;
+using Box.V2;
+using Box.V2.Models;
 
 namespace BUFHelper
 {
     public class UserFolders
     {
+        private BoxClient client;
 
+        public UserFolders(BoxClient client)
+        {
+            this.client = client;
+        }
+
+        public System.Collections.Generic.List<Box.V2.Models.BoxUser> getUsers()
+        {
+            var users = client.UsersManager.GetEnterpriseUsersAsync(limit: 20);
+            users.Wait();
+            return users.Result.Entries;
+        }
+
+        public BoxEventCollection<BoxEnterpriseEvent> getNewUserEvents()
+        {
+            var events = client.EventsManager.EnterpriseEventsAsync(limit: 20, eventTypes: new[] { "NEW_USER" });
+            return events.Result;
+        }
+
+        public BoxEventCollection<BoxEnterpriseEvent> getGroupAddEvents()
+        {
+            var events = client.EventsManager.EnterpriseEventsAsync(limit: 20, eventTypes: new[] { "GROUP_ADD_USER" });
+            return events.Result;
+        }
+
+        public void makeFoldersForUser(string uid)
+        {
+            var userGroups = getUserGroups(uid);
+            userGroups.Entries.ForEach((BoxGroupMembership membership) =>
+            {
+                if (membership.Group.Id == "123")
+                {
+                    
+                }
+            });
+        }
+
+        public BoxCollection<BoxGroupMembership> getUserGroups(string uid)
+        {
+            return client.GroupsManager.GetAllGroupMembershipsForUserAsync(uid, autoPaginate: true).Result;
+        }
+
+        public BoxCollection<BoxGroupMembership> getGroupUsers(string gid)
+        {
+            return client.GroupsManager.GetAllGroupMembershipsForGroupAsync(gid, autoPaginate: true).Result;
+        }
+
+        public BoxCollection<BoxItem> getFolderContents(string id)
+        {
+            return client.FoldersManager.GetFolderItemsAsync(id, 1000, autoPaginate: true).Result;
+        }
     }
 }
--- BoxUserFolders/Program.cs
+++ BoxUserFolders/Program.cs
@@ -1,12 +1,108 @@
 ﻿using System;
+using System.IO;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using Box.V2.Config;
+using Box.V2.JWTAuth;
+using Box.V2;
+using BUFHelper;
+using Box.V2.Models;
 
 namespace BoxUserFolders
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootcmd = new RootCommand
+            {
+                new Option<FileInfo>(new[] {"--box-auth-file", "-f" }, description: "Box JWT JSON file") {IsRequired = true },
+            };
+
+            //rootcmd.Handler = CommandHandler.Create((IConsole console, FileInfo boxAuthFile) =>
+            //{
+            //    if (boxAuthFile != null)
+            //    {
+            //        console.Out.Write(boxAuthFile.FullName);
+            //        console.Out.Write("\n");
+            //    }
+            //    else
+            //    {
+            //        console.Out.Write("please provide an auth file\n");
+            //    }
+            //});
+
+            var getUsersCmd = new Command("list-users");
+            getUsersCmd.Handler = CommandHandler.Create((IConsole console, FileInfo boxAuthFile) =>
+            {
+                var UF = new UserFolders(makeClient(boxAuthFile));
+                var list = UF.getUsers();
+                list.ForEach((BoxUser user) =>
+                {
+                    console.Out.Write($"{user.Id}: {user.Name}\n");
+                });
+            });
+
+            var getNewCmd = new Command("list-new-users");
+            getNewCmd.Handler = CommandHandler.Create((IConsole console, FileInfo boxAuthFile) =>
+            {
+                var UF = new UserFolders(makeClient(boxAuthFile));
+                var list = UF.getNewUserEvents();
+                //list.Entries.ForEach((BoxEnterpriseEvent e) =>
+                //{
+                //    console.Out.Write($"{e.}: {e.Name}\n");
+                //});
+
+                var ad = list.Entries[0].AdditionalDetails;
+
+                foreach (var key in ad.Keys)
+                {
+                    console.Out.Write($"{key}: {ad[key]}\n");
+                }
+            });
+
+            var getTokenCmd = new Command("get-token");
+            getTokenCmd.Handler = CommandHandler.Create((IConsole console, FileInfo boxAuthFile) =>
+            {
+                console.Out.Write((new BoxJWTAuth(BoxConfig.CreateFromJsonFile(boxAuthFile.OpenRead()))).AdminToken() + "\n");
+            });
+
+            var getFolderListingCmd = new Command("ls") {
+                new Argument<string>("folder-id")
+            };
+            getFolderListingCmd.Handler = CommandHandler.Create((IConsole console, FileInfo boxAuthFile, string folderId) =>
+            {
+                try
+                {
+                    var UF = new UserFolders(makeClient(boxAuthFile, "13792189662"));
+                    var list = UF.getFolderContents(folderId);
+                    list.Entries.ForEach((BoxItem item) =>
+                    {
+                        console.Out.Write($"{item.Id}: {item.Name}\n");
+                    });
+                }
+                catch (Exception e)
+                {
+                    console.Error.Write(e.ToString() + "\n");
+                    throw;
+                }
+                
+            });
+
+            rootcmd.AddCommand(getUsersCmd);
+            rootcmd.AddCommand(getNewCmd);
+            rootcmd.AddCommand(getTokenCmd);
+            rootcmd.AddCommand(getFolderListingCmd);
+
+            return rootcmd.InvokeAsync(args).Result;
+        }
+
+
+        private static BoxClient makeClient(FileInfo authFile, string asUser = null)
+        {
+            var boxcfg = BoxConfig.CreateFromJsonFile(authFile.OpenRead());
+            var jwt = new BoxJWTAuth(boxcfg);
+            return jwt.AdminClient(jwt.AdminToken(), asUser: asUser);
         }
     }
 }
